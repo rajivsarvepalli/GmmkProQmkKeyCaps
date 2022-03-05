ba9ce650f92ffc6e50bab1a20a54d3e5ca26a33a
Gehl Benjámin Konrád <gehlbenji@gmail.com>
2022-03-05 21:02:59 +0100

    Add Space Cadet Shift

--- benjamingehl/keymap.c
+++ benjamingehl/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc          PgUp
     // Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgDn
     // Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             Home
-    // Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
+    // LSPO              Z        X        C        V        B        N        M        ,        .        ?                 RSPC     Up       End
     // Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
 
     [BASE] = LAYOUT(
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC, KC_UP,   KC_END,
         KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
