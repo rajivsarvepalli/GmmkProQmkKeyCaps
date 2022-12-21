c330443c907dd677445df0a21e7d92656b619d58
Rajiv Sarvepalli <rajiv@ericavijay.net>
2022-12-21 00:38:04 +0000

    Create keymap for GMMK Pro

--- benjamingehl/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#ifdef RGB_MATRIX_ENABLE
-    #define RGB_DISABLE_WHEN_USB_SUSPENDED true
-#endif // RGB_MATRIX_ENABLE
---
+++ rajivsarvepalli/config.h
@@ -0,0 +1,7 @@
+#define RGB_MATRIX_TIMEOUT 900000 // number of milliseconds to wait until rgb automatically turns off
+#define RGB_DISABLE_WHEN_USB_SUSPENDED 1 // turn off effects when suspended
+#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR // Sets the default mode, if none has been set
+#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 0 // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
