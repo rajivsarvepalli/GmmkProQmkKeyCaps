de9d4e0e2207c910a63cbdbcf618614d663b67fb
rajivsarvepalli <rajiv@sarvepalli.net>
2024-08-20 20:01:53 -0400

    Add update qmk firmware files

--- rajivsarvepalli/keymap.c
+++ rajivsarvepalli/keymap.c
@@ -43,40 +43,34 @@ enum custom_keycodes {
     PRT_EM,
     PSCR,
     DEL_WRD,
-    MUL_PST,
-    SIDE_LT
+    MUL_PST
 };
 
-const int SIZE = 8;
-int leftSideLEDs[] = { 67, 70, 73, 76, 80, 83, 87, 91 };
-int rightSideLEDs[] = { 68, 71, 74, 77, 81, 84, 88, 92 };
-
-bool is_win_mode = false;
-bool sides_lights_on = false;
+bool is_win_mode = true;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
-    // ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc          Del
-    // Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
-    // Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
+    // ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc          PgUp
+    // Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgDn
+    // Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             Home
     // LSPO              Z        X        C        V        B        N        M        ,        .        ?                 RSPC     Up       End
     // Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
 
     [BASE] = LAYOUT(
         KC_ESC,  KC_BRID, KC_BRIU, KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MPRV, KC_MNXT, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, PSCR,             KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DELETE,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        MUL_PST, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
+        MUL_PST, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
         KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RGUI, MO(FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [FN] = LAYOUT(
-        _______, KC_F1,   KC_F2,   _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          _______,
-        NK_TOGG, RGB_TOG, RGB_VAI, RGB_VAD, RGB_HUI, RGB_SAI, RGB_SAD, RGB_M_P, SIDE_LT, _______, _______, _______, _______, DEL_WRD,          _______,
+        QK_BOOT, KC_F1,   KC_F2,   _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          _______,
+        NK_TOGG, RGB_TOG, RGB_VAI, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, DEL_WRD,          _______,
         _______, _______, _______, PRT_EM,  _______, _______, _______, _______, _______, OS_CHG,  _______, _______, _______, _______,          _______,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          QK_BOOT,          _______,
+        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
         KC_LSFT,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_RSFT, RGB_MOD, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
@@ -124,72 +118,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         encoder_counterclockwise();
     }
 
-    return false;
+    return true;
 }
 #endif // ENCODER_ENABLE
-
-#ifdef RGB_MATRIX_ENABLE
-
-void keyboard_post_init_user(void) {
-    rgb_matrix_enable_noeeprom();
-    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-}
-
-void suspend_power_down_kb(void) {
-    rgb_matrix_set_suspend_state(true);
-    suspend_power_down_user();
-}
-
-void suspend_wakeup_init_kb(void) {
-    rgb_matrix_set_suspend_state(false);
-    suspend_wakeup_init_user();
-}
-
-void set_caps_led_indicator(void) {
-    rgb_matrix_set_color(LED_CAPS, 200, 0, 0);
-}
-
-void set_fn_led_overlay(uint8_t led_min, uint8_t led_max) {
-    uint8_t layer = get_highest_layer(layer_state);
-
-
-    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
-        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
-            uint8_t index = g_led_config.matrix_co[row][col];
-
-            if (index >= led_min && index <= led_max && index != NO_LED &&
-            keymap_key_to_keycode(layer, (keypos_t){col,row}) != KC_TRNS) {
-                rgb_matrix_set_color(index, 0, 191, 255);
-            }
-        }
-    }
-
-    if (is_win_mode) {
-        rgb_matrix_set_color(LED_O, 200, 0, 0);
-    } else {
-        rgb_matrix_set_color(LED_O, 0, 0, 200);
-    }
-}
-
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-
-    if (host_keyboard_led_state().caps_lock) {
-        set_caps_led_indicator();
-    }
-
-    if (get_highest_layer(layer_state) > BASE) {
-        set_fn_led_overlay(led_min, led_max);
-    }
-
-    if (sides_lights_on) {
-        for (int i = 0; i < SIZE; i++) {
-            rgb_matrix_set_color(leftSideLEDs[i], 255, 255, 255);
-            rgb_matrix_set_color(rightSideLEDs[i], 255, 255, 255);
-        }
-    }
-
-    return false;
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
@@ -222,18 +153,70 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             tap_code16(keycode);
             break;
         }
-        case SIDE_LT: {
-            sides_lights_on = !sides_lights_on;
-            if (!sides_lights_on) {
-                rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
-            }
-            break;
-        }
     }
 
     return true;
 }
 
-#endif // RGB_MATRIX_ENABLE
+#ifdef RGB_MATRIX_ENABLE
+void keyboard_post_init_user(void) {
+    rgb_matrix_enable_noeeprom();
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_set_color_all(255, 137, 0);
+}
 
-// buggy as in several keys are missing (new Fn keys not added, old upto 5 old version is still there)
+void suspend_power_down_kb(void) {
+    rgb_matrix_set_suspend_state(true);
+    suspend_power_down_user();
+}
+
+void suspend_wakeup_init_kb(void) {
+    rgb_matrix_set_suspend_state(false);
+    suspend_wakeup_init_user();
+}
+
+void set_caps_led_indicator(void) {
+    HSV current_hsv = rgb_matrix_get_hsv();
+    HSV caps_hsv = {V_RED, current_hsv.s, min(current_hsv.v + 50, 255)};
+    RGB caps_rgb = hsv_to_rgb(caps_hsv);
+    rgb_matrix_set_color(LED_CAPS, caps_rgb.r, caps_rgb.g, caps_rgb.b);
+}
+
+void set_fn_led_overlay(uint8_t led_min, uint8_t led_max) {
+    uint8_t layer = get_highest_layer(layer_state);
+
+    HSV current_hsv = rgb_matrix_get_hsv();
+    HSV fn_hsv = {(current_hsv.h + 10) % 255, min(current_hsv.s + 50, 255), min(current_hsv.v + 50, 255)};
+    RGB fn_rgb = hsv_to_rgb(fn_hsv);
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+            uint8_t index = g_led_config.matrix_co[row][col];
+
+            if (index >= led_min && index <= led_max && index != NO_LED &&
+            keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                rgb_matrix_set_color(index, fn_rgb.r, fn_rgb.g, fn_rgb.b);
+            }
+        }
+    }
+
+    if (is_win_mode) {
+        rgb_matrix_set_color(LED_O, 200, 0, 0);
+    } else {
+        rgb_matrix_set_color(LED_O, 0, 0, 200);
+    }
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        set_caps_led_indicator();
+    }
+
+    if (get_highest_layer(layer_state) > BASE) {
+        set_fn_led_overlay(led_min, led_max);
+    }
+
+    return false;
+}
+
+#endif // RGB_MATRIX_ENABLE
